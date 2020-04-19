f274e1303a6f97cb03c23c37823c276b6bffd9fb
Henry Sunuc <henry.viaguate@gmail.com>
2020-04-18 22:02:20 -0600

    deploy method

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hola a todos
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi, i'm Henry Alvarez
+        </a>
+      </header>
     </div>
   );
 }
