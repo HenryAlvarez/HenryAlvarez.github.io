3f6a811c453e76cc186e6ead40c7064697b0e26e
Henry Sunuc <henry.viaguate@gmail.com>
2020-04-18 21:50:09 -0600

    Updates

---
+++ precache-manifest.bd2a7ed74537d7b8e759e3d8278ed1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68454dd2e8b899f81007278f50e6085f",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/index.html"
+  },
+  {
+    "revision": "cc3c4f1bd8b0fcd6fb9c",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6cd051871f41812a9ed",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/js/2.6c428f77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/js/2.6c428f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3c4f1bd8b0fcd6fb9c",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/js/main.cd66c527.chunk.js"
+  },
+  {
+    "revision": "f497687737ab07b0da92",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/js/runtime-main.ed854c28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HenryAlvarez/HenryAlvarez.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
